c79f200978efc3f853976642f1343f5765a434b8
Zsolt Ébel <zsolt_ebel@epam.com>
2017-11-04 15:50:41 +0100

    Spring Boot project was created

---
+++ src/main/java/com/example/bank/BankWebappApplication.java
@@ -0,0 +1,12 @@
+package com.example.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebappApplication.class, args);
+	}
+}
